bd22d015f8d4ce7503d0a3ace48483cdcff2071f
parte <partaevil@gmail.com>
2025-06-14 22:28:59 +0300

    add cards

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,29 @@
-// @todo: Темплейт карточки
+const cardTemplate = document.querySelector('#card-template').content;
 
-// @todo: DOM узлы
+const placesList = document.querySelector('.places__list');
 
-// @todo: Функция создания карточки
+function deleteCard(event) {
+  const card = event.target.closest('.card');
+  card.remove();
+}
 
-// @todo: Функция удаления карточки
+function createCard(cardData) {
+  const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
 
-// @todo: Вывести карточки на страницу
+  const cardImage = cardElement.querySelector('.card__image');
+  const cardTitle = cardElement.querySelector('.card__title');
+  const deleteButton = cardElement.querySelector('.card__delete-button');
+
+  cardImage.src = cardData.link;
+  cardImage.alt = cardData.name;
+  cardTitle.textContent = cardData.name;
+
+  deleteButton.addEventListener('click', deleteCard);
+  
+  return cardElement;
+}
+
+initialCards.forEach(function(cardData) {
+  const cardElement = createCard(cardData);
+  placesList.append(cardElement);
+});
